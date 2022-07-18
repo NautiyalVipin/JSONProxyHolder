1ce0a1f6129d1409adebda727b1422a7222a7b16
vipinnautiyal <blifeology@gmail.com>
2022-07-18 16:57:31 +0530

    final

--- client/proxyholder/src/App.js
+++ client/proxyholder/src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import ClipLoader from "react-spinners/ClipLoader";
 import { useQuery } from 'react-query'
 import Card from "./Card"
 import "./App.css"
@@ -14,30 +15,33 @@ function App() {
         else return res.status(404)
     }
 
-    const { data, isLoading, isError} = useQuery(["posts", value], fetchQuery, { retry: false })
- 
+    const { data, isLoading, isError } = useQuery(["posts", value], fetchQuery, { retry: false })
+
     return (
-        <div className='text-center bg-sky-800'>
+        <div className='text-center border-2 border-sky-800 bg-sky-800'>
 
-      
-        <div className='bg-blue-200 border-3 border-gray-400 p-4 text-center font-mono relative rounded '>
-           <div className='sticky top-0 border  border-gray-400  left-0 right-0 bg-blue-100 rounded-xl'>
-            <h1  className="text-3xl  p-2 margin-2 font-bold">JSONProxyHolder!</h1>
-            <input className='w-80 focue:outline-0 p-2 text-xl border border-gray-400 rounded-xl m-4'
-                type="text"
-                placeholder="write the relative URL here to get data"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                />
+
+            <div className='bg-blue-200 border-3 border-gray-400 p-4 text-center font-mono relative rounded '>
+                <div className='sticky top-0 border  border-gray-400  left-0 right-0 bg-blue-100 rounded-xl'>
+                    <h1 className="text-3xl text-stone-800 p-2 margin-2 font-bold">JSONProxyHolder</h1>
+                    <input className='w-80 focue:outline-none focus:ring focus:border-black  p-2 text-xl  rounded-xl m-4'
+                        type="text"
+                        placeholder="write the relative URL here to get data"
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                    />
                 </div>
-            <div className='flex flex-wrap gap-4  p-10 rounded'>
-            {data?.id ? (<Card title={data.title} />) : isLoading ? (<h1> ...loading </h1>) : isError ? (<h1> ...error </h1>) : data?.map(e => {
-                return (<Card body={e.body} id={e.id} title={e.title} />)
+                <div className='flex flex-wrap gap-4  p-10 rounded'>
+                    {data?.id ? (<Card title={data.title} />) : isLoading ? (<div type="button" className="  rounded-2xl text-center w-full "> <ClipLoader color='black'></ClipLoader>
 
-            })}
+                    </div>) : isError ? (<div type="button" className=" bg-blue-200 text-2xl rounded-2xl text-center w-full "> Try Searching a different query!
+                    </div>) : data?.map(e => {
+                        return (<Card key={e.id} body={e.body} id={e.id} title={e.title} />)
+
+                    })}
+                </div>
             </div>
-        </div>
-<div className='text-2xl font-bold  rounded-xl p-2 m-2 '>	&#169;CodedamnProjects2022</div>
+            <div className='text-2xl font-bold text-stone-800 font-mono rounded-xl p-2 m-2 '>	&#169;codeDamnProjects2022</div>
         </div>
     )
 }
--- client/proxyholder/src/Card.js
+++ client/proxyholder/src/Card.js
@@ -1,18 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+import ClickAwayListener from 'react-click-away-listener';
+const Card = (props) => {
+    const [value, setValue] = useState(false)
 
-const Card = (props) =>{
+    const LargeDiv = () => {
+        return (<ClickAwayListener onClickAway={()=>setValue(e=>!e)}>
+            <div className="fixed overflow-y-auto  top-20 p-14 inset-x-9 p-4 w-600 h-96 border border-gray-800 z-10 bg-sky-900 rounded-3xl text-white"><button onClickAway={()=>alert("Click away works")} onClick={() => setValue(e => !e)} className=" rounded-full bg-gray-200 w-16 text-black absolute right-0 top-0 m-2 p-1">X</button>
+            <div className="">
+                <h3 className="text-2xl  m-2 font-bold capitalize">{props.id}: {props.title}</h3>
+                <p className="font-light text-xl p-10">{props.body}</p>
 
-
-return   <div className="flex-auto p-4 w-60 bg-blue-100 border border-gray-400 hover:bg-sky-800 hover:text-white shadow  rounded-lg">
-                <div className="rounded-lg">
-                    <h3 className="text-xl m-2 font-bold capitalize">{props.id}: {props.title}</h3>
-                    <p  className="font-light">{props.body}</p>
-                    <a href={props.body} target="_blank">
-                        {/* <button>Full Story</button> */}
-                    </a>
-                </div>
+            </div>
 
         </div>
+         </ClickAwayListener>)
+    }
+
+    if (value) return <LargeDiv />
+    return <div onClick={() => setValue(true)} className="flex-auto p-4 w-60 bg-blue-100 border border-gray-400 transition-all ease-in-out hover:translate  hover:bg-sky-900 hover:text-white rounded-lg">
+        <div className="">
+            <h3 className="text-xl m-2 font-bold capitalize">{props.id}: {props.title}</h3>
+        </div>
+
+    </div>
 
 }
 
